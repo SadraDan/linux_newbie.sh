8c101f2968a7b73b388fc1fc70822110cb873ea2
Sadra Daneshmand <sadrathescientist@gmail.com>
2021-08-09 19:40:51 +0430

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,6 +1,6 @@
 echo ---------------
 
-echo 'hello and welcome to the automized bash file for your new linux distro!'
+echo hello and welcome to the automized bash file for your new linux distro!
 
 echo ---------------
 
@@ -9,12 +9,12 @@ sudo apt-get update -y ; sudo apt-get upgrade -y ; sudo apt-get autoremove -y ; 
 
 echo ---------------
 
-echo 'as you were drinking your coffee,' 
-echo 'your linux distro got updated, and autocleaned as well! :)' 
+echo as you were drinking your coffee, 
+echo your linux distro got updated, and autocleaned as well! 
 
 echo ---------------
 
-echo 'now I\'m going to install gnome-tweak for ya!'
+echo now I am going to install gnome-tweak for ya!
 
 echo ---------------
 
@@ -22,8 +22,8 @@ sudo apt-get install gnome-tweak-tool -y
 
 echo ---------------
 
-echo 'Time to enable the firewall!'
-echo 'Just need you to type "y" for me'
+echo Time to enable the firewall!
+echo Just need you to type "y" for me
 
 echo ---------------
 
@@ -31,7 +31,7 @@ sudo ufw enable
 
 echo ---------------
 
-echo 'time to install synaptic!'
+echo time to install synaptic!
 
 echo ---------------
 
@@ -39,7 +39,7 @@ sudo apt-get install synaptic -y
 
 echo ---------------
 
-echo 'Now I am going to improve your your battery performance!' 
+echo Now I am going to improve your your battery performance!
 
 echo ---------------
 
@@ -48,16 +48,14 @@ sudo systemctl enable tlp
 
 echo ---------------
 
-echo 'time to install snap so you get more freedom!'
+echo time to install snap so you get more freedom!
 
 sudo apt update -y ; sudo apt install snapd -y 
 sudo apt-get update -y ; sudo apt-get upgrade -y ; sudo apt-get autoremove -y ; sudo apt-get autoclean -y ; sudo apt-get clean -y 
 
 echo ---------------
 
-echo 'your Linux is ready to go!'
-echo 'see ya!'
+echo your Linux is ready to go!
+echo see ya!
 
 echo ---------------
-
-
